941cb081bbb4b6d24be0c1848c552fc7bf005ab6
Gabriel Ojeda <gabojeda.o@gmail.com>
2022-08-04 17:10:31 -0400

    Servicio de email conectado

--- src/components/shared/ContactForm/index.jsx
+++ src/components/shared/ContactForm/index.jsx
@@ -1,3 +1,5 @@
+import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form"
 import {
     ContactContainer,
     ContactTitle,
@@ -10,6 +12,26 @@ import {
 } from "./styles"
 
 const ContactForm = () => {
+
+    const { register, handleSubmit, errors, reset } = useForm()
+
+    const onSubmit = data => {
+        emailjs.send('service_pabz39i', 'template_7z3853g', data, '6A7puWAZzRlHTWFH8')
+            .then(res => {
+                console.log('Email successfully sent!')
+            }).catch(err => {
+                console.log('Email failed to send!')
+            }
+            )
+        reset({
+            from_name: "",
+            from_email: "",
+            message: ""
+
+        })
+
+    }
+    console.log(errors);
     return (
         <ContactContainer>
             <ContactHeader>
@@ -18,10 +40,10 @@ const ContactForm = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </ContactSubtitle>
             </ContactHeader>
-            <ContactFormContainer>
-                <InputText type="text" placeholder="Ingresa tu nombre" required />
-                <InputText type="text" placeholder="Ingresa tu correo" required />
-                <InputArea type="area" placeholder="Ingresa tu mensaje" required />
+            <ContactFormContainer onSubmit={handleSubmit(onSubmit)}>
+                <InputText type="text" placeholder="Ingresa tu nombre" {...register("from_name", { required: true, min: 4, pattern: /[a-zA-Z ]{2,254}/i })} />
+                <InputText type="email" placeholder="Ingresa tu correo" {...register("from_email", { required: true, pattern: /^\S+@\S+$/i })} />
+                <InputArea type="area" placeholder="Ingresa tu mensaje" {...register("message", { required: true })} />
                 <InputButton type="submit" >Enviar</InputButton>
             </ContactFormContainer>
 
