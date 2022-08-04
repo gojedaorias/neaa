d9474b311276453c49d271b49c0ea580aa53aa76
Gabriel Ojeda <gabojeda.o@gmail.com>
2022-08-04 18:05:56 -0400

    arreglando deploy

--- src/components/Testimonial/index.jsx
+++ src/components/Testimonial/index.jsx
@@ -5,8 +5,9 @@ import {
   TestimonialText,
   TestimonialTitle,
 } from './style';
+import logoBootstrap from '../../assets/logos/Bootstrap.svg';
 import logoGoogle from '../../assets/logos/google.png';
-import logoSpotify from '../../assets/logos/spotify.png';
+import logoSpotify from '../../assets/logos/Spotify.svg';
 
 export const Testimonial = () => {
   return (
@@ -16,7 +17,7 @@ export const Testimonial = () => {
         Nos esmeramos en ofrecer soluciones para darle valor a tus procesos de producción y logramos que tus proyectos sean más eficaces y económicos.
       </TestimonialText>
       <TestimonialLogos>
-        <img src={logoGoogle} alt='logo de Bootstrap' />
+        <img src={logoBootstrap} alt='logo de Bootstrap' />
         <img src={logoGoogle} alt='Logo de Google' />
         <img src={logoSpotify} alt='logo de Spotify' />
       </TestimonialLogos>
